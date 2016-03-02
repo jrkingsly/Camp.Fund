0d580fff62529b89b1636e2584d08b1c290e7458
Justin Rosen <justin.rosen@rrpartners.com>
2016-03-02 14:28:09 -0800

    Init Commit

---
+++ Gruntfile.js
@@ -0,0 +1,54 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		
+		uglify: {
+            options: {
+                mangle: false
+            },
+            my_target: {
+                files: {
+                    'dist/js/jquery.all.min.js': ['build/js/jquery-1.9.1.js','build/js/jquery.validate.min.js', 'build/js/video.js', 'build/js/script.js']
+                }
+            }
+        },
+		
+		cssmin: {
+            target: {
+                files: {
+                    'dist/css/main.min.css': ['build/css/custom.css','build/css/lang.css','build/css/video-js.css']
+                }
+            }
+        },
+		
+		htmlmin: {
+			dist: {
+				options: {
+					removeComments: true,
+					collapseWhitespace: true
+				},
+				files: {
+					'dist/index.html': 'build/index.html'
+				}
+			}
+		},
+		
+		copy: {
+			main: {
+				files: [
+					{src: 'build/css/bootstrap.min.css', dest: 'dist/css/bootstrap.min.css'},
+					{src: 'build/js/bootstrap.min.js', dest: 'dist/js/bootstrap.min.js'},
+					{expand: true, cwd: 'build/img', src: '**', dest: 'dist/img/'}
+				]
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+
+	grunt.registerTask('default', ['uglify','cssmin','htmlmin','copy']);
+
+};
---
+++ build/js/script.js
@@ -0,0 +1,35 @@
+$(function(){
+	
+	$('#accordion').find('.accordion-toggle').click(function(){
+		//Hide the other panels
+		$(".accordion-content").not($(this).next()).slideUp('fast');
+		//Expand or collapse this panel
+		$(this).next().slideToggle('fast');	
+	});
+	     
+	$('#thumbs').delegate('img', 'click', function() {
+	    $('#largeImage').attr('src', $(this).attr('src').replace('thumb', 'large'));
+	    window.location.hash = $('#thumbs img').index(this)+1;
+	});
+	
+	
+	wlh = window.location.hash[1];
+	if (!isNaN(wlh)) {
+	    $('#largeImage').attr('src', $('#thumbs img').eq(wlh-1).attr('src').replace('thumb', 'large'));
+	}	
+	
+	$('#box').hide();
+	
+	$('#clickme').on('click', function(e) {
+	    e.preventDefault();
+	    $(this).text(
+	    	$(this).text() === 'Hide' ? 'Read More' : 'Hide'
+	    );
+	    $('#box').slideToggle();
+	});
+});	
+	
+	
+	
+	
+ 
